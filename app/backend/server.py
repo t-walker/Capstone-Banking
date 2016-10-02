d168e6d7cbf0acf5e0b868acca38c0f13620e26b
Tyler Walker <twalker@eecs.wsu.edu>
2016-10-02 16:24:27 -0700

    Angular is served by Nginx and Flask is served on its own server

--- app/backend/server.py
+++ app/backend/server.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('app/index.html')
-
-if __name__ == '__main__':
-  app.run()
+  return "HelloWorld"
