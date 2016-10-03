29a3570b0b3f29b5669f314f5106f9be03dacbb0
Tyler Walker <twalker@eecs.wsu.edu>
2016-10-03 00:57:23 -0700

    Updating angular branch as well as incorporating the DB

--- app/backend/server.py
+++ app/backend/server.py
@@ -1,7 +1,14 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://pgdbuser:pgdbpassword@db/varda"
 
-@app.route('/')
+db = SQLAlchemy(app)
+db.create_all()
+
+@app.route('/api/')
 def index():
-  return "HelloWorld"
+  obj = {} 
+  obj['status'] = "working"
+  return jsonify(**obj)
