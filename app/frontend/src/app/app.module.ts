df1b82eca8d9ba61e4816bb93f3aed7757d100b0
Tyler Walker <twalkerweb@gmail.com>
2016-10-21 15:46:36 -0700

    fixing some errors by removing includes

--- app/frontend/src/app/app.module.ts
+++ app/frontend/src/app/app.module.ts
@@ -2,8 +2,6 @@ import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from "./app.component";
-import {NavbarComponent} from "./shared/navbar/navbar.component";
-import {FooterComponent} from "./shared/footer/footer.component";
 import {AboutComponent} from "./about/components/about.component";
 import {HomeComponent} from "./home/components/home.component";
 
--- app/frontend/src/app/app.routing.ts
+++ app/frontend/src/app/app.routing.ts
@@ -1,5 +1,4 @@
 import {Routes, RouterModule} from "@angular/router";
-import {TaskListComponent} from "./todo/components/task-list.component";
 import {ModuleWithProviders} from "@angular/core";
 
 import {AboutComponent} from "./about/components/about.component";
@@ -7,9 +6,8 @@ import {HomeComponent} from "./home/components/home.component";
 
 const appRoutes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
-    {path: 'about', component: AboutComponent, data: {title: 'About'}}
+    {path: 'about', component: AboutComponent, data: {title: 'About'}},
     {path: 'home', component: HomeComponent, data: {title: 'Home'}}
-
 ];
 
 export const appRoutingProviders: any[] = [];
