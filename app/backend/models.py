0bda477784231c1865b2730e9cfbc4fd1076ec4b
Tyler Walker <twalker@eecs.wsu.edu>
2016-10-25 21:06:10 -0700

    adding marshmallow and others

--- app/backend/models.py
+++ app/backend/models.py
@@ -1,4 +1,4 @@
-from server import db
+from server import db, ma
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # MODELS
@@ -11,6 +11,8 @@ class User(db.Model):
     last_name = db.Column(db.String(100), unique=False)
     email = db.Column(db.String(120), unique=True)
     password_hash = db.Column(db.String(1000), unique=False)
+
+    accounts = db.relationship("Account", backref="user", lazy="dynamic")
 
     def __init__(self, username, email, first_name, last_name, password):
         self.username = username
@@ -25,21 +27,31 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-    def to_json(self):
-        return dict(id=self.id,first_name=self.first_name, last_name=self.last_name)
 
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     account_type = db.Column(db.String(string_maximum), unique=False)
-    transactions = relationship("Transaction", back_populates="account")
+    transactions = db.relationship("Transaction", backref="account", lazy="dynamic")
 
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    account = relationship("Account", back_populates="transaction")
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
     tx_type = db.Column(db.String(1), unique=False)
     tx_from = db.Column(db.String(string_maximum), unique=False)
     tx_to = db.Column(db.String(string_maximum), unique=False)
     amount = db.Column(db.Float, unique=False)
+
+
+# Schemas
+class UserSchema(ma.Schema):
+    class Meta:
+        model = User
+
+class AccountSchema(ma.Schema):
+    class Meta:
+        model = Account
+
+class TransactionSchema(ma.Schema):
+    class Meta:
+        model = Transaction
--- app/backend/server.py
+++ app/backend/server.py
@@ -2,6 +2,7 @@ from time import sleep # Wait for the DB to be ready.
 
 from flask import Flask, send_file, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -9,9 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://pgdbuser:pgdbpassword@db/var
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['DEBUG'] = True
 
-sleep(5)
-
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 from models import *
 
@@ -36,6 +36,37 @@ def create_user():
     except:
         db.session.rollback()
     return request.data
+
+@app.route('/api/transaction', methods=['POST'])
+def create_transaction():
+    # {'account_id': '', 'tx_type': '', 'tx_from': '', tx_to: '', amount: ''}
+    tx = Transaction(**request.json)
+
+    try:
+        db.session.add(tx)
+        db.session.commit()
+    except:
+        db.session.rollback()
+    return request.data
+
+@app.route('/api/account/<int:account_id>/transactions', methods=['GET'])
+def get_account_transactions(account_id):
+    transaction_schema = TransactionSchema()
+
+    account = db.session.query(Account).filter_by(id=account_id).first()
+    transactions = account.transactions.all()
+    print(transactions)
+
+    values = []
+
+    print(values)
+
+    for tx in transactions:
+        values.append(transaction_schema.dump(tx).data)
+
+    return jsonify(values)
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
