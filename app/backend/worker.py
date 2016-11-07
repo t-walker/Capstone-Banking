76b100a26372a481a60c76892295f7c03b65dcf6
Tyler Walker <twalkerweb@gmail.com>
2016-11-06 18:04:29 -0800

    localhost -> redis

--- app/backend/worker.py
+++ app/backend/worker.py
@@ -1,10 +1,13 @@
 import os
+import time
 from celery import Celery
 
 env=os.environ
-CELERY_BROKER_URL=env.get('CELERY_BROKER_URL','redis://localhost:6379'),
-CELERY_RESULT_BACKEND=env.get('CELERY_RESULT_BACKEND','redis://localhost:6379')
+CELERY_BROKER_URL=env.get('CELERY_BROKER_URL','redis://redis:6379'),
+CELERY_RESULT_BACKEND=env.get('CELERY_RESULT_BACKEND','redis://redis:6379')
 
-celery= Celery('tasks')
+celery= Celery('tasks',
+                broker=CELERY_BROKER_URL,
+                backend=CELERY_RESULT_BACKEND)
 
-app.config_from_object('celeryconfig')
+celery.config_from_object('celeryconfig')
--- app/task/tasks.py
+++ app/task/tasks.py
@@ -3,8 +3,8 @@ import time
 from celery import Celery
 
 env=os.environ
-CELERY_BROKER_URL=env.get('CELERY_BROKER_URL','redis://localhost:6379'),
-CELERY_RESULT_BACKEND=env.get('CELERY_RESULT_BACKEND','redis://localhost:6379')
+CELERY_BROKER_URL=env.get('CELERY_BROKER_URL','redis://redis:6379'),
+CELERY_RESULT_BACKEND=env.get('CELERY_RESULT_BACKEND','redis://redis:6379')
 
 celery= Celery('tasks',
                 broker=CELERY_BROKER_URL,
