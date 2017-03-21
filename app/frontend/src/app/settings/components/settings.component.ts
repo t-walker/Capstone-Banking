1c7d535ca0fbeeb962196eb8731985c21078e0fe
Tyler Cruz <tymicruz@gmail.com>
2017-03-20 21:56:13 -0700

    edit fields

--- app/frontend/src/app/settings/components/settings.component.ts
+++ app/frontend/src/app/settings/components/settings.component.ts
@@ -11,19 +11,105 @@ export class SettingsComponent implements OnInit {
   private fname = "relyt";
   private lname = "zurc";
   private username = "tcruz";
-  private password = "jello";
+  private password = "jellooo";
+  private email = "t@email.com";
+  private passwordShow = "***";
+  private defaultDeposit = "Checking";
+
+  private account: number;
+  private accounts = [{id: 1, account_type: "Checking", total: 100}, {id: 2, account_type: "Savings", total: 500}];
+
+  private editingName = false;
+  private editingUsername = false;
+  private editingPassword = false;
+  private editingEmail = false;
+  private editingDefaultDeposit = false;
+
+
 
   constructor(private userService: UserService) { }
 
   ngOnInit() {
     
     console.log("Settings component initialized ...");
+
+    this.passwordShow = "";
+
+    for (var i = 0; i < this.password.length; i++) {
+
+        this.passwordShow += "*";
+    }
+
   }
 
 
-  onEdit() {
+  onEdit(field) {
 
-    console.log("hi");
+    switch (field) {
+      case 'name':
+        // code...
+        this.editingName = true;
+        break;
+      case 'username':
+        // code...
+        this.editingUsername = true;
+
+        break;
+      case 'email':
+        // code...
+        this.editingEmail = true;
+
+        break;
+      case 'password':
+        // code...
+        this.editingPassword = true;
+
+        break;
+      case 'defaultDeposit':
+      this.editingDefaultDeposit = true;
+
+      
+      default:
+        // code...
+        break;
+    }
+  }
+
+  onCancel(field) { 
+
+     switch (field) {
+      case 'name':
+        // code...
+        this.editingName = false;
+        break;
+      case 'username':
+        // code...
+        this.editingUsername = false;
+
+        break;
+      case 'email':
+        // code...
+        this.editingEmail = false;
+
+        break;
+      case 'password':
+        // code...
+        this.editingPassword = false;
+
+        break;
+      
+      case 'defaultDeposit':
+        this.editingDefaultDeposit = false;
+        break;
+      default:
+        // code...
+        break;
+    }
+
+  }
+
+  onSave(field) {
+
   }
 
   deactivate() {
